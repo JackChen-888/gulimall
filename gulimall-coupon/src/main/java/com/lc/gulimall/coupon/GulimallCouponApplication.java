7f3ef514bc0489eea38042d8b1d2a5a583519830
liuchen <liuchen525@126.com>
2020-11-24 13:24:32 +0800

    引入nacos作为注册中心

--- gulimall-coupon/src/main/java/com/lc/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/lc/gulimall/coupon/GulimallCouponApplication.java
@@ -2,12 +2,15 @@ package com.lc.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//开启服务注册发现客户端
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GulimallCouponApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
 
 }
