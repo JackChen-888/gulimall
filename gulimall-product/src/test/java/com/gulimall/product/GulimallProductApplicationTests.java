1a92b7e0e494c1df82cf4c261c64c70046b869a6
liuchen <liuchen525@126.com>
2021-11-04 10:09:55 +0800

    测试提交

--- gulimall-product/src/test/java/com/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/gulimall/product/GulimallProductApplicationTests.java
@@ -17,10 +17,6 @@ class GulimallProductApplicationTests {
 
     @Test
     void contextLoads() {
-//        BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setName("华为");
-//        brandService.save(brandEntity);
-//        System.out.println("保存成功");
 
         List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
         list.forEach(System.out :: println);
